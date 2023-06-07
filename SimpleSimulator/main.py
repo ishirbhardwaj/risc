52b01e01b50a8caf1391b7d5fdde725a99150462
Ishir Bhardwaj <ishirbhardwaj@gmail.com>
2023-06-07 06:47:47 +0530

    Simulator

--- SimpleSimulator/main.py
+++ SimpleSimulator/main.py
@@ -8,7 +8,7 @@ flag = True
 #     if len(line) > 0:
 #         binary_instructions.append(line)
 #     else:
-# break
+#         break
 
 while True:
     try:
@@ -28,98 +28,109 @@ def var_length_binary(x, n):
     return f"{(n - len(x)) * '0'}{x}"
 
 
+def decimal(x):
+    x = int(x)
+    output = 0
+    power = 1
+    while x > 0:
+        output = output + (x % 10) * power
+        power *= 2
+        x //= 10
+    return output
+
+
 while flag:
     instruction = binary_instructions[pc]
     op_code = instruction[:5]
     mem_control = False
     if op_code == "00000":
         rf[7] = 0
-        rf[int(instruction[7:10], 2)] = (
-            rf[int(instruction[10:13], 2)] + rf[int(instruction[13:], 2)]
+        rf[decimal(instruction[7:10])] = (
+            rf[decimal(instruction[10:13])] + rf[decimal(instruction[13:])]
         )
-        if rf[int(instruction[7:10], 2)] > ((2**16) - 1):
-            rf[int(instruction[7:10], 2)] = 0
+        if rf[decimal(instruction[7:10])] > ((2**16) - 1):
+            rf[decimal(instruction[7:10])] = 0
             rf[7] = 8
     elif op_code == "00001":
         rf[7] = 0
-        rf[int(instruction[7:10], 2)] = (
-            rf[int(instruction[10:13], 2)] - rf[int(instruction[13:], 2)]
+        rf[decimal(instruction[7:10])] = (
+            rf[decimal(instruction[10:13])] - rf[decimal(instruction[13:])]
         )
-        if rf[int(instruction[7:10], 2)] < 0:
-            rf[int(instruction[7:10], 2)] = 0
+        if rf[decimal(instruction[7:10])] < 0:
+            rf[decimal(instruction[7:10])] = 0
             rf[7] = 8
     elif op_code == "00010":
         rf[7] = 0
-        rf[int(instruction[6:9], 2)] = int(instruction[9:], 2)
+        rf[decimal(instruction[6:9])] = decimal(instruction[9:])
     elif op_code == "00011":
+        rf[decimal(instruction[10:13])] = rf[decimal(instruction[13:])]
         rf[7] = 0
-        rf[int(instruction[10:13], 2)] = rf[int(instruction[13:], 2)]
     elif op_code == "00100":
         rf[7] = 0
-        rf[int(instruction[6:9], 2)] = int(
-            binary_instructions[int(instruction[9:], 2)], 2
+        rf[decimal(instruction[6:9])] = decimal(
+            binary_instructions[decimal(instruction[9:])]
         )
     elif op_code == "00101":
         rf[7] = 0
-        binary_instructions[int(instruction[9:], 2)] = var_length_binary(
-            rf[int(instruction[6:9], 2)], 16
+        binary_instructions[decimal(instruction[9:])] = var_length_binary(
+            rf[decimal(instruction[6:9])], 16
         )
     elif op_code == "00110":
         rf[7] = 0
-        rf[int(instruction[7:10], 2)] = (
-            rf[int(instruction[10:13], 2)] * rf[int(instruction[13:], 2)]
+        rf[decimal(instruction[7:10])] = (
+            rf[decimal(instruction[10:13])] * rf[decimal(instruction[13:])]
         )
-        if rf[int(instruction[7:10], 2)] > ((2**16) - 1):
-            rf[int(instruction[7:10], 2)] = 0
+        if rf[decimal(instruction[7:10])] > ((2**16) - 1):
+            rf[decimal(instruction[7:10])] = 0
             rf[7] = 8
     elif op_code == "00111":
         rf[7] = 0
-        rf[0] = rf[int(instruction[10:13], 2)] // rf[int(instruction[13:], 2)]
-        rf[1] = rf[int(instruction[10:13], 2)] % rf[int(instruction[13:], 2)]
+        rf[0] = rf[decimal(instruction[10:13])] // rf[decimal(instruction[13:])]
+        rf[1] = rf[decimal(instruction[10:13])] % rf[decimal(instruction[13:])]
         if rf[4] == 0:
             rf[0] = 0
             rf[1] = 0
             rf[7] = 8
     elif op_code == "01000":
         rf[7] = 0
-        rf[int(instruction[6:9], 2)] = int(
-            f"{'0' * int(instruction[9:], 2)}{bin(rf[int(instruction[6:9], 2)])[2:]}"[
+        rf[decimal(instruction[6:9])] = decimal(
+            f"{'0' * decimal(instruction[9:])}{bin(rf[decimal(instruction[6:9])])[2:]}"[
                 :16
-            ],
-            2,
+            ]
         )
     elif op_code == "01001":
         rf[7] = 0
-        rf[int(instruction[6:9], 2)] = int(
-            f"{bin(rf[int(instruction[6:9], 2)])[2:]}{'0' * int(instruction[9:], 2)}"[
+        rf[decimal(instruction[6:9])] = decimal(
+            f"{bin(rf[decimal(instruction[6:9])])[2:]}{'0' * decimal(instruction[9:])}"[
                 -16:
-            ],
-            2,
+            ]
         )
     elif op_code == "01010":
         rf[7] = 0
-        rf[int(instruction[7:10], 2)] = (
-            rf[int(instruction[10:13], 2)] ^ rf[int(instruction[13:], 2)]
+        rf[decimal(instruction[7:10])] = (
+            rf[decimal(instruction[10:13])] ^ rf[decimal(instruction[13:])]
         )
     elif op_code == "01011":
         rf[7] = 0
-        rf[int(instruction[7:10], 2)] = (
-            rf[int(instruction[10:13], 2)] | rf[int(instruction[13:], 2)]
+        rf[decimal(instruction[7:10])] = (
+            rf[decimal(instruction[10:13])] | rf[decimal(instruction[13:])]
         )
     elif op_code == "01100":
         rf[7] = 0
-        rf[int(instruction[7:10], 2)] = (
-            rf[int(instruction[10:13], 2)] & rf[int(instruction[13:], 2)]
+        rf[decimal(instruction[7:10])] = (
+            rf[decimal(instruction[10:13])] & rf[decimal(instruction[13:])]
         )
     elif op_code == "01101":
         rf[7] = 0
-        rf[int(instruction[10:13], 2)] = int("1" * 16, 2) - rf[int(instruction[13:], 2)]
+        rf[decimal(instruction[10:13])] = (
+            decimal("1" * 16) - rf[decimal(instruction[13:])]
+        )
     elif op_code == "01110":
-        if rf[int(instruction[10:13], 2)] == rf[int(instruction[13:], 2)]:
+        if rf[decimal(instruction[10:13])] == rf[decimal(instruction[13:])]:
             rf[7] = 1
-        elif rf[int(instruction[10:13], 2)] > rf[int(instruction[13:], 2)]:
+        elif rf[decimal(instruction[10:13])] > rf[decimal(instruction[13:])]:
             rf[7] = 2
-        elif rf[int(instruction[10:13], 2)] < rf[int(instruction[13:], 2)]:
+        elif rf[decimal(instruction[10:13])] < rf[decimal(instruction[13:])]:
             rf[7] = 4
     elif op_code == "01111":
         mem_addr = instruction[9:]
@@ -152,7 +163,7 @@ while flag:
     pc += 1
 
     if mem_control is not False:
-        pc = int(mem_addr, 2)
+        pc = decimal(mem_addr)
 
 for i in binary_instructions:
     print(i)
